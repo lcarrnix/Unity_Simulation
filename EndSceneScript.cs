63af188f677497dd89d7c6ab332381dd552a3829
lcarrnix <lcarrnix@users.noreply.github.com>
2017-05-30 16:23:09 -0400

    Add files via upload
    Changes were made to these files 
    EndSceneScript - adding "Exit" button to completely quit the application
    LoginButton - cleaned up script, implemented passwordToggle
    InterfaceScript - cleaned up script, implemented all UI elements used, added Settings menu, added Help menu, and how to exit each of those

---
+++ EndSceneScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class EndSceneScript : MonoBehaviour {
+	public Button exitBtn; 
+
+	// Use this for initialization
+	void Start () {
+		exitBtn = exitBtn.GetComponent<Button> ();
+		exitBtn.enabled = true; //user is able to exit immediately
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ExitGame() //ending session- jumps to exit screen
+	{
+		Application.Quit ();
+	}
+
+}
