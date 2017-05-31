a3c12e13cda666c6b22fa4804445abaae669e4da
lcarrnix <lcarrnix@users.noreply.github.com>
2017-05-31 11:05:46 -0400

    Update StandController.cs

--- StandController.cs
+++ StandController.cs
@@ -6,6 +6,8 @@ public class StandController : MonoBehaviour {
 	//controls optional stand component of the system
 	//*** not currently implemented into the simulation
 
+	//may be useful in future once design of environemnt and player are incorporated
+
 	public GameObject Stand; //self
 	public Rigidbody rb; //for collisions and parking system in place
 
