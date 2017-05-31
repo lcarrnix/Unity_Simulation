5e3d601bb2fb62727a47ea7996d5f93c1a9ef18a
lcarrnix <lcarrnix@users.noreply.github.com>
2017-05-31 11:05:24 -0400

    Update SystemController.cs

--- SystemController.cs
+++ SystemController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SystemController : MonoBehaviour {
+	//controls overall robot system, if optional stand component is implemented
+	//*** not currently used in the simulation
+	
+	//may be useful in future once design of environemnt and player are incorporated
 
 	public GameObject System; //self
 	public Rigidbody rb; //for collisions and parking system in place
 
 	void Start()
 	{
-		Debug.Log ("hello");
 		rb = GetComponent<Rigidbody> ();
 	}
 
