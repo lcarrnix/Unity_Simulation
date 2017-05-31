96ed1afed0652af6cfef5d9836135f759022af2d
lcarrnix <lcarrnix@users.noreply.github.com>
2017-05-31 11:01:44 -0400

    Update StandController.cs

--- StandController.cs
+++ StandController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StandController : MonoBehaviour {
+	//controls optional stand component of the system
+	//*** not currently implemented into the simulation
 
 	public GameObject Stand; //self
 	public Rigidbody rb; //for collisions and parking system in place
 
 	void Start()
 	{
-		Debug.Log ("hello");
 		rb = GetComponent<Rigidbody> ();
 	}
 
