bd3e605c3c6e408ee1e92f610a800bb640ac578f
amandabrummett <amandamb@email.sc.edu>
2017-08-02 16:40:18 -0400

    made changes to tracking user position
    Old user data is now deleted from the file so that each time the simulation is run the new data is not just added on to the old data. Now the new test run has its own file (so we need to make sure we save the file somewhere else between each run of the simulation!)

--- UserPosition.cs
+++ UserPosition.cs
@@ -9,30 +9,31 @@ public class UserPosition : MonoBehaviour
 {
 
 	string fileName = "userPosition.XML"; //saves data to an XML file called userPosition
+	string currentFile = @"C:\Users\Rebecca\Documents\Roll a Ball example\userPosition.XML"; //file path may change****
 	int time = 0;
 	int interval = 50; //interval for checking the users position (every 50 units)
-
 
 	// Use this for initialization
 	void Start ()
 	{
-	 //do nothing here
+		File.Delete (currentFile); //deletes old user data by deleting the file so MAKE SURE TO SAVE FILE SOMEWHERE BETWEEN TESTS!!
+		Debug.Log ("Deleted old data");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		
 		if (time > interval) {
 			float x = transform.position.x; //the user's current x position
-			//float y = transform.position.y; //the user's current y position
 			float z = transform.position.z; //the user's current z position
-
-			StreamWriter sw = File.AppendText(fileName); //or use File.AppendText to just add all data to one file
-			sw.WriteLine (x + " " + z); //outputs a line with the user's current x position seperated from z position with single space
-			//sw.WriteLine ("Z: " + z); //outputs a line with the user's current z position
-			sw.Close (); 
+		
+			StreamWriter sw = File.AppendText (fileName);
+			sw.WriteLine (x + " " + z);
+			sw.Close ();
 			Debug.Log ("writing to file");
 			time = 0;
+
 		} else {
 			time++;
 		}
